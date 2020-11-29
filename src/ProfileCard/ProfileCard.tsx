5e65401ced8a649cb0072520bc432e22a374d9e3
simsimjae <simsimjae@naver.com>
2020-11-29 10:02:20 +0900

    소개 수정

--- src/ProfileCard/ProfileCard.tsx
+++ src/ProfileCard/ProfileCard.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState, useCallback } from "react";
-import { ProfileCardStyled } from "./ProfileCard.styles";
-import GoogleFontLoader from "react-google-font-loader";
-import axios from "axios";
-import root from "window-or-global";
+import React, { useEffect, useState, useCallback } from 'react';
+import { ProfileCardStyled } from './ProfileCard.styles';
+import GoogleFontLoader from 'react-google-font-loader';
+import axios from 'axios';
+import root from 'window-or-global';
 
 export interface ProfileCardProps {
   width: string;
@@ -30,8 +30,8 @@ const ProfileCard = (props: ProfileCardProps) => {
     (async () => {
       try {
         const Axios = axios.create({
-          baseURL: "https://api.github.com/users",
-          auth: { username: "d93bbed6abb05249b568", password: "d29ba5732f3672cd20a16f4f16900666c29a09b0" },
+          baseURL: 'https://api.github.com/users',
+          auth: { username: 'd93bbed6abb05249b568', password: 'd29ba5732f3672cd20a16f4f16900666c29a09b0' },
         });
         const profile = (await Axios.get(`/${id}`)).data;
         setProfile(profile);
@@ -53,36 +53,36 @@ const ProfileCard = (props: ProfileCardProps) => {
       {root && root.document && (
         <GoogleFontLoader
           fonts={[
-            { font: "Roboto", weights: [400, 700] },
-            { font: "Noto Sans KR", weights: [400, 700] },
+            { font: 'Roboto', weights: [400, 700] },
+            { font: 'Noto Sans KR', weights: [400, 700] },
           ]}
         />
       )}
       {(props.isSSR || isMount) && (
         <ProfileCardStyled width={width} height={height} background={background} color={color} onClick={props.onClickCard || onClickCard} isCircleImage={isCircleImage}>
-          <div className="left">
-            <div className="image_area">
-              <img src={(profile && profile.avatar_url) || profileSrc} alt="profile image" />
+          <div className='left'>
+            <div className='image_area'>
+              <img src={(profile && profile.avatar_url) || profileSrc} alt='profile image' />
             </div>
           </div>
-          <div className="right">
-            <div className="naming">
-              <h3 className="name">{(profile && profile.name) || name}</h3>
-              <span className="id">@{(profile && profile.login) || id}</span>
+          <div className='right'>
+            <div className='naming'>
+              <h3 className='name'>{(profile && profile.name) || name}</h3>
+              <span className='id'>@{(profile && profile.login) || id}</span>
             </div>
-            <p className="introduce">{(profile && profile.bio) || introduce}</p>
-            <div className="infos">
-              <div className="info">
-                <span className="info_title">Repository</span>
-                <span className="info_count">{(profile && profile.public_repos) || repositoryCount}</span>
+            <p className='introduce'>{(profile && profile.bio) || introduce}</p>
+            <div className='infos'>
+              <div className='info'>
+                <span className='info_title'>Repository</span>
+                <span className='info_count'>{(profile && profile.public_repos) || repositoryCount}</span>
               </div>
-              <div className="info">
-                <span className="info_title">Following</span>
-                <span className="info_count">{(profile && profile.following) || followingCount}</span>
+              <div className='info'>
+                <span className='info_title'>Following</span>
+                <span className='info_count'>{(profile && profile.following) || followingCount}</span>
               </div>
-              <div className="info">
-                <span className="info_title">Followers</span>
-                <span className="info_count">{(profile && profile.followers) || followerCount}</span>
+              <div className='info'>
+                <span className='info_title'>Followers</span>
+                <span className='info_count'>{(profile && profile.followers) || followerCount}</span>
               </div>
             </div>
           </div>
@@ -93,17 +93,17 @@ const ProfileCard = (props: ProfileCardProps) => {
 };
 
 ProfileCard.defaultProps = {
-  width: "600px",
-  height: "280px",
-  background: "#FFFFFF",
-  color: "#26273B",
-  name: "Jae Cheol Sim",
-  id: "simsimjae",
-  profileSrc: "https://placehold.it/500x500",
+  width: '600px',
+  height: '280px',
+  background: '#FFFFFF',
+  color: '#26273B',
+  name: 'Jae Cheol Sim',
+  id: 'simsimjae',
+  profileSrc: 'https://placehold.it/500x500',
   repositoryCount: 23,
   followingCount: 314,
   followerCount: 487,
-  introduce: "이곳에는 자기소개를 입력해주세요 세줄이 넘어가면 자동으로 말줄임 처리 됩니다. 단 세줄로 자신을 표현해보세요. made by simsimjae",
+  introduce: 'hello, my name is jae cheol sim',
   isSSR: false,
   isCircleImage: false,
 };
